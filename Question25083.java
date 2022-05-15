e399ec94ed19d2a93d2f2a792c2b3b6c2734730d
Caramel1004 <Caramel1004.js@gmail.com>
2022-05-15 18:14:10 +0900

    2022-05-15

---
+++ Question25083.java
@@ -0,0 +1,13 @@
+public class Question25083 {
+    public static void main(String[] args) {
+
+        String line1 = "         ,r'\"7";
+        String line2 = "r`-_   ,'  ,/";
+        String line3 = " \\. \". L_r'";
+        String line4 = "   `~\\/";
+        String line5 = "      |";
+
+         
+        System.out.println(line1+"\n"+line2+"\n"+line3+"\n"+line4+"\n"+line5+"\n"+line5);
+    }
+}
