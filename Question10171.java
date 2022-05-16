8abb9dc915c468f8ef569336f7069ad4f453c7c9
Caramel1004 <Caramel1004.js@gamil.com>
2022-05-16 23:46:20 +0900

    2022-05-16(고양이 출력)

---
+++ Question10171.java
@@ -0,0 +1,16 @@
+// 문제 링크 : https://www.acmicpc.net/problem/10171
+// 아래 예제와 같이 고양이를 출력하시오.
+// \    /\
+//  )  ( ')
+// (  /  )
+//  \(__)|
+
+public class Question10171 {
+    public static void main(String[] args) {
+
+        String readLine = "\\    /\\\n )  ( ')\n(  /  )\n \\(__)|";
+
+        System.out.println(readLine);
+
+    }
+}
