e149854273a63e163abcd4ddce1d2a7e566fe9fb
Caramel1004 <Caramel1004.js@gamil.com>
2022-05-16 23:45:58 +0900

    2022-05-16(구구단)

---
+++ Question2739.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+// 문제 링크 : https://www.acmicpc.net/problem/2739
+// 문제
+// N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
+
+// 입력
+// 첫째 줄에 N이 주어진다. N은 1보다 크거나 같고, 9보다 작거나 같다.
+
+// 출력
+// 출력형식과 같게 N*1부터 N*9까지 출력한다.
+public class Question2739 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int num = input.nextInt();
+
+        input.close();
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(num + " * " + i + " = " + num * i);
+        }
+    }
+}
