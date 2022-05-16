88e3a4efe511e63c6f71ccc382a422b003540c49
Caramel1004 <Caramel1004.js@gamil.com>
2022-05-16 23:46:43 +0900

    2022-05-16(출력문제)

---
+++ Question10718.java
@@ -0,0 +1,14 @@
+//문제 링크 : https://www.acmicpc.net/problem/10718
+// 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+
+public class Question10718 {
+    public static void main(String[] args) {
+
+        String str = "강한친구 대한육군";
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println(str);
+        }
+
+    }
+}
