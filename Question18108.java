b4b1695e27896779c0c86ef3aab93d9388dedf32
Caramel1004 <Caramel1004.js@gamil.com>
2022-05-19 22:19:00 +0900

    2022-05-19

---
+++ Question18108.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Question18108 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int bulgiYear = input.nextInt();
+
+        int seogiYear = bulgiYear - 543;
+
+        System.out.println(seogiYear);
+
+    }
+}
