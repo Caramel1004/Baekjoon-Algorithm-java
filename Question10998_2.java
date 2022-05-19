5c6c0785acf0c49f3038f7b4de7e8fa8497b861c
Caramel1004 <Caramel1004.js@gamil.com>
2022-05-19 22:16:39 +0900

    2022-05-19

---
+++ Question10998_2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Question10998_2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int integ1 = input.nextInt();
+        int integ2 = input.nextInt();
+
+        int multi = integ1 * integ2;
+
+        System.out.println(multi);
+    }
+}
